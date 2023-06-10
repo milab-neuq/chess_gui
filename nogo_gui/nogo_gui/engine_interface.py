2a1a8e30c3c2f2ce84bc89ee9c3a85b413ada2af
Dongxinyu <92635232+dxyznb@users.noreply.github.com>
2023-06-10 19:09:44 +0800

    Add files via upload

---
+++ nogo_gui/nogo_gui/engine_interface.py
@@ -0,0 +1,12 @@
+class engine_interface:
+    def __init__(self):
+        # todo
+        pass
+
+    def load_engine(self):
+        # todo
+        pass
+
+    def unload_engine(self):
+        # todo
+        pass
