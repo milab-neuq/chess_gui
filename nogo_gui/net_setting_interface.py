82c79e49ba2e5b80feb22dc63b69187e5a447a9a
Dongxinyu <92635232+dxyznb@users.noreply.github.com>
2023-06-10 18:31:45 +0800

    Add files via upload

---
+++ nogo_gui/net_setting_interface.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QWidget
+
+
+class net_setting_interface(QWidget):
+    def __init__(self):
+        super().__init__()
+        # todo
+        pass
+
+    def net_setting(self):
+        self.show()
+        # todo
+        pass
+
+    def start_net_vs(self):
+        # todo
+        pass
+
+    def close_net_vs(self):
+        # todo
+        pass
